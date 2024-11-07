4d1eff40946b2617982146af46da0c570d7f9ee5
Jerry Hor <jhor@u81z04s03.42kl.edu.my>
2024-11-07 16:01:50 +0800

    attempt one

---
+++ ft_isdigit.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhor <jhor@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 19:31:59 by jhor              #+#    #+#             */
+/*   Updated: 2024/11/07 15:32:30 by jhor             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+/** 
+ * @brief check digit
+ * @param c character
+ * @return 1 is true or 0 false
+*/
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int main()
+{
+	int c = 'a';
+	printf("%d", ft_isdigit(c));
+}
